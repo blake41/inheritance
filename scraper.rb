6e558fbeb611543bdda9b6c76fddc0597ce5807a
Blake Johnson <blake41@gmail.com>
2014-05-13 12:18:07 -0400

    simpler version suggested by george

--- scraper.rb
+++ scraper.rb
@@ -1,17 +1,26 @@
-
+require 'pry'
 class Scraper
   attr_accessor :response
 
-  def yield_links(block)
-    parse_structure.map do |link|
-      block.call(link)
-    end
+  # def yield_links
+
+  # end
+
+  def parse
+    JSON.parse(response)
   end
 
-  def get_links(&block)
+  def get_links
+    # invisible block sitting in this method
+    # we call yield, we'll "call" that block
+    # do |book|
+    #   book["book_details"].first["book_image"]
+    # end
     response = RestClient.get(construct_url)
-    self.response = JSON.parse(response)
-    yield_links(block)
+    self.response = parse
+    parse_structure.map do |link|
+      yield link
+    end
   end
 
 end
